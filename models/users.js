9bc6985ddbd32e5b56318e41e14fd3bb1bcd9054
Joriiss <jorisvaux@gmail.com>
2022-12-04 11:56:14 +0100

    project done

--- models/users.js
+++ models/users.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose');
 
+// user model schema
 const userSchema = new mongoose.Schema({
-    
+        // email must be unique and match an email syntax
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Not a valid email address']
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        age : {
+            type: Number,
+            required: true,
+            min: 13
+        }
 })
 
 module.exports = mongoose.model('User', userSchema)
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,81 @@
-const express = require('express');
-const User = require('../models/users');
+const express = require("express");
+const User = require("../models/users");
 const router = express.Router();
 
 //Post Method
-router.post('/users', async (req, res) => {})
+router.post("/users", async (req, res) => {
+  const name = req.body.name; //get the datas
+  const email = req.body.email;
+  const age = req.body.age;
+
+  const data = new User({
+    name: name,
+    email: email,
+    age: age,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 //Get Method
-router.get('/users', async (req, res) => {})
+//get all users
+router.get("/users", async (req, res) => {
+  try {
+    const usersData = await User.find({});
+    res.status(200).json(usersData);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-router.get('/users/:id', async (req, res) => {})
+//get users by id
+router.get("/users/:id", async (req, res) => {
+  const id = req.params.id; //get the id of the user from url
+  try {
+    const userData = await User.findById(id);
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(400).json({ message: "no users found with this id" });
+  }
+});
 
 //Put Method
-router.put('/users/:id', async (req, res) => {})
+//update a user
+router.put("/users/:id", async (req, res) => {
+  const id = req.params.id; //get the id of the user from url
+  const name = req.body.name; //get the datas
+  const email = req.body.email;
+  const age = req.body.age;
+
+  const data = {
+    name: name,
+    email: email,
+    age: age,
+  };
+
+  try {
+    const newData = await User.findByIdAndUpdate(id, data);
+    res.status(200).json(await User.findById(id));
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 //Delete
-router.delete('/users/:id', async (req, res) => {})
+router.delete("/users/:id", async (req, res) => {
+    const id = req.params.id; //get the id of the user from url
+
+    try {
+        const userData = await User.deleteOne({_id: id});
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(400).json({ message: "no users found with this id" });
+    }
+});
 
 module.exports = router;
